fb4fbc63a9edfe1c5883fddf57bc88ea92eb2588
johannes <johannes@nudgenudge.eu>
2006-02-02 21:18:41 +0000

    cocoa application scaffold

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  punakea
+//
+//  Created by Johannes Hoffart on 02.02.06.
+//  Copyright __MyCompanyName__ 2006. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
