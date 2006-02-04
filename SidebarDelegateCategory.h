256cd3352266bd3b885bcf896552477ce240b988
daniel <daniel@nudgenudge.eu>
2006-02-04 22:09:11 +0000

    some stuff for sidebar

---
+++ SidebarDelegateCategory.h
@@ -0,0 +1,15 @@
+//
+//  SidebarDelegateCategory.h
+//  punakea
+//
+//  Created by Daniel on 04.02.06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "Controller.h"
+
+
+@interface Controller (SidebarDelegateCategory)
+
+@end
---
+++ SidebarDelegateCategory.m
@@ -0,0 +1,24 @@
+//
+//  SidebarDelegateCategory.m
+//  punakea
+//
+//  Created by Daniel on 04.02.06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import "SidebarDelegateCategory.h"
+
+
+@implementation Controller (SidebarDelegateCategory)
+- (int)numberOfRowsInTableView:(NSTableView *)tableView
+{
+    return 4;
+}
+
+- (id)tableView:(NSTableView *)tableView
+      objectValueForTableColumn:(NSTableColumn *)tableColumn
+      row:(int)row
+{
+	return @"Steve";
+}
+@end
