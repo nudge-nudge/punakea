67afc17593657349ad4c46b95f02d3576920e10f
johannes <johannes@nudgenudge.eu>
2006-02-06 08:47:38 +0000

    first steps towards the tagger

---
+++ TaggerInterface.h
@@ -0,0 +1,16 @@
+//
+//  TaggerInterface.h
+//  punakea
+//
+//  Created by Johannes Hoffart on 05.02.06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TaggerInterface : NSObject {
+
+}
+
+@end
---
+++ TaggerInterface.m
@@ -0,0 +1,16 @@
+//
+//  TaggerInterface.m
+//  punakea
+//
+//  Created by Johannes Hoffart on 05.02.06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import "TaggerInterface.h"
+#import "Matador.h"
+
+@implementation TaggerInterface
+-(void)writeSpotlightComment {
+	
+}
+@end
