876df6745f705f2c309ba6dae82c8976c450fff6
johannes <johannes@nudgenudge.eu>
2006-02-07 21:30:23 +0000

    tags are set as kMDItemKeywords

--- TaggerInterface.m
+++ TaggerInterface.m
@@ -9,9 +9,17 @@
 #import "TaggerInterface.h"
 #import "Matador.h"
 
+//for CFArray
+#import <Carbon/Carbon.h>
+
+
 @implementation TaggerInterface
 -(void)writeSpotlightComment {
 	NSLog(@"rennt weiter");
-	[[Matador sharedInstance] setAttributeForFileAtPath:@"/Users/daniel/Desktop/punakea_test" name:@"kMDItemFinderComment" value:@"danieltest"];
+	NSMutableArray *tags = [NSMutableArray new];
+	[tags addObject:@"testtag"];
+	[tags addObject:@"ein schoener tag"];
+	NSLog([tags objectAtIndex:0]);
+	[[Matador sharedInstance] setAttributeForFileAtPath:@"/Users/darklight/Desktop/punakea_test" name:@"kMDItemKeywords" value:tags];
 }
 @end
