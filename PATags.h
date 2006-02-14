85c054ece0089c7354cb97765c870bedc3bc0d9b
johannes <johannes@nudgenudge.eu>
2006-02-14 21:11:42 +0000

    syntax ;)

--- PATags.h
+++ PATags.h
@@ -12,7 +12,7 @@
 @interface PATags : NSObject {
 	NSMutableArray* relatedTags;
 	NSMutableArray* activeTags;
-	NSNotificationCenter *nf;
+	NSNotificationCenter* nf;
 }
 
 //collection type might change, don't depend on it too heavily
