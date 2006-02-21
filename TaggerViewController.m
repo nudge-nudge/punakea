97def9069629afd9358e24acbf7f6f94a5b7fae5
johannes <johannes@nudgenudge.eu>
2006-02-21 13:14:13 +0000

    integrated query to Controller.m adjusted some of the stuff (tagging should work now)

--- TaggerViewController.m
+++ TaggerViewController.m
@@ -7,7 +7,18 @@
 }
 
 - (IBAction)setTagsForFile:(id)sender {
-	NSArray* tags = [[tagList stringValue] componentsSeparatedByString:@","];
+	NSArray* tagNames = [[tagList stringValue] componentsSeparatedByString:@","];
+	
+	NSEnumerator *e = [tagNames objectEnumerator];
+	NSString *tagName;
+	NSMutableArray *tags = [[NSMutableArray alloc] init];
+	
+	while (tagName = [e nextObject]) {
+		PATag *tag = [[PATag alloc] initWithName:tagName];
+		[tags addObject:tag];
+		[tag release];
+	}		
+	
 	NSLog(@"trying to write %@ to %@",tags,[filePath stringValue]);
 	[ti addTagsToFile:tags filePath:[filePath stringValue]];
 }
