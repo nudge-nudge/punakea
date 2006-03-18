48b6256250eb6a5335da1efa2bf174e11387b0c0
johannes <johannes@nudgenudge.eu>
2006-03-18 07:23:21 +0000

    forgot to add PATag.m again

---
+++ PATag.m
@@ -0,0 +1,140 @@
+//
+//  PATag.m
+//  punakea
+//
+//  Created by Johannes Hoffart on 15.02.06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import "PATag.h"
+
+@implementation PATag 
+
+#pragma mark init
+
+//designated initializer
+- (id)initWithName:(NSString*)aName 
+{
+	return nil;
+}
+
+//NSCoding
+- (id)initWithCoder:(NSCoder*)coder 
+{
+	return nil;
+}
+
+- (void)encodeWithCoder:(NSCoder*)coder 
+{
+
+}
+
+#pragma mark accessors
+- (void)setName:(NSString*)aName 
+{
+
+}
+
+- (void)setQuery:(NSString*)aQuery 
+{
+
+}
+
+- (void)incrementClickCount 
+{
+
+}
+
+- (void)incrementUseCount 
+{
+
+}
+
+- (void)setCurrentBestTag:(PATag*)aTag
+{
+
+}
+
+- (PATag*)currentBestTag 
+{
+	return nil;
+}
+
+- (NSString*)name 
+{
+	return nil;
+}
+
+- (NSString*)query 
+{
+	return nil;
+}
+
+- (NSCalendarDate*)lastClicked 
+{
+	return nil;
+}
+
+- (NSCalendarDate*)lastUsed 
+{
+	return nil;
+}
+
+- (unsigned long)clickCount 
+{
+	return 0;
+}
+
+- (unsigned long)useCount 
+{
+	return 0;
+}
+
+- (float)absoluteRating
+{
+	return 0;
+}
+
+- (float)relativeRating
+{	
+	return 0;
+}
+
+#pragma mark drawing
+- (NSMutableDictionary*)viewAttributes
+{
+	return nil;
+}
+
+- (void)drawInRect:(NSRect)rect withAttributes:(NSDictionary*)attributes
+{
+
+}
+
+- (NSSize)sizeWithAttributes:(NSDictionary*)attributes
+{
+	return NSMakeSize(0,0);
+}
+
+- (void)setHighlight:(BOOL)flag 
+{	
+
+}
+
+#pragma mark euality testing
+- (BOOL)isEqual:(id)other 
+{
+	return NO;
+}
+
+- (BOOL)isEqualToTag:(PATag*)otherTag 
+{
+	return NO;
+}
+
+- (unsigned)hash 
+{
+	return 0;
+}
+
+@end
