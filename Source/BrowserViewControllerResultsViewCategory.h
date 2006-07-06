8fa2d433280b6d6113029e3ecec1eec973228511
daniel <daniel@nudgenudge.eu>
2006-07-06 08:46:06 +0000

    added file

---
+++ Source/BrowserViewControllerResultsViewCategory.h
@@ -0,0 +1,19 @@
+//
+//  BrowserViewControllerResultsViewCategory.h
+//  punakea
+//
+//  Created by Daniel on 06.07.06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "BrowserViewController.h"
+#import "PAResultsGroupCell.h"
+#import "PAResultsItemCell.h"
+#import "PAResultsMultiItem.h"
+#import "PAResultsMultiItemCell.h"
+
+
+@interface BrowserViewController (ResultsViewCategory) 
+
+@end
