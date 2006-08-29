9868b7f5a6b2bc1d8b219b0b68441c8ab40dc888
johannes <johannes@nudgenudge.eu>
2006-08-29 07:01:19 +0000

    typeahead now has a searchfield

--- Source/PASidebarWindow.m
+++ Source/PASidebarWindow.m
@@ -29,7 +29,17 @@ double const SHOW_DELAY = 0.2;
                             styleMask:NSBorderlessWindowMask
                               backing:bufferingType defer:flag];
 	
+    //Set the background color to clear so that (along with the setOpaque call below) we can see through the parts
+    //of the window that we're not drawing into
+    [self setBackgroundColor: [NSColor clearColor]];
+    //This next line pulls the window up to the front on top of other system windows.  This is how the Clock app behaves;
+    //generally you wouldn't do this for windows unless you really wanted them to float above everything.
     [self setLevel: NSStatusWindowLevel];
+    //Let's start with no transparency for all drawing into the window
+    [self setAlphaValue:1.0];
+    //but let's turn off opaqueness so that we can see through the parts of the window that we're not drawing into
+    [self setOpaque:NO];	
+	
 	[self setAcceptsMouseMovedEvents:YES];
 	
 	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
