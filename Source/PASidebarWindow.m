bea9d81891433baa828ad9d6507cada94fdb7000
johannes <johannes@nudgenudge.eu>
2006-09-15 09:11:00 +0000

    first shot at sidebar

--- Source/PASidebarWindow.m
+++ Source/PASidebarWindow.m
@@ -31,7 +31,7 @@ double const SHOW_DELAY = 0.2;
 	
     //Set the background color to clear so that (along with the setOpaque call below) we can see through the parts
     //of the window that we're not drawing into
-    //[self setBackgroundColor: [NSColor clearColor]];
+    [self setBackgroundColor: [NSColor whiteColor]];
     //This next line pulls the window up to the front on top of other system windows.  This is how the Clock app behaves;
     //generally you wouldn't do this for windows unless you really wanted them to float above everything.
     [self setLevel: NSStatusWindowLevel];
@@ -147,7 +147,7 @@ double const SHOW_DELAY = 0.2;
 				break;
 		}
 
-		[self setAlphaValue:1.0];
+		[self setAlphaValue:0.75];
 
 		[self setFrame:newRect display:YES animate:animate];
 		[self setExpanded:YES];
