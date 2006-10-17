f3744fd774a83419c919d15940e76e3b7ded2454
johannes <johannes@nudgenudge.eu>
2006-10-17 18:06:00 +0000

    another update to splitview. seems to work fine (fixing #102)

---
+++ Source/PABrowserSplitView.h
@@ -0,0 +1,16 @@
+//
+//  PABrowserSplitView.h
+//  punakea
+//
+//  Created by Johannes Hoffart on 17.10.06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PABrowserSplitView : NSSplitView {
+
+}
+
+@end
---
+++ Source/PABrowserSplitView.m
@@ -0,0 +1,19 @@
+//
+//  PABrowserSplitView.m
+//  punakea
+//
+//  Created by Johannes Hoffart on 17.10.06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import "PABrowserSplitView.h"
+
+
+@implementation PABrowserSplitView
+
+- (float)dividerThickness
+{
+	return 1.0;
+}
+
+@end
