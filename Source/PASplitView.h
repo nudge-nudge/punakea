e11016e898728607c712c670b7bbf11b0526cbf7
daniel <daniel@nudgenudge.eu>
2007-03-29 12:35:09 +0000

    added custom generic splitview

---
+++ Source/PASplitView.h
@@ -0,0 +1,16 @@
+//
+//  PASplitView.h
+//  punakea
+//
+//  Created by Daniel on 29.03.07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PASplitView : NSSplitView {
+
+}
+
+@end
---
+++ Source/PASplitView.m
@@ -0,0 +1,24 @@
+//
+//  PASplitView.m
+//  punakea
+//
+//  Created by Daniel on 29.03.07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "PASplitView.h"
+
+
+@implementation PASplitView
+
+- (float)dividerThickness
+{
+	return 1.0;
+}
+
+- (void)drawDividerInRect:(NSRect)aRect
+{
+	// override to do nothing
+}
+
+@end
