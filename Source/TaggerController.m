ccf9553517d7433b7b778fe7c662bc48363645a4
johannes <johannes@nudgenudge.eu>
2007-05-30 20:05:53 +0000

    compiles again

--- Source/TaggerController.m
+++ Source/TaggerController.m
@@ -333,6 +333,8 @@ adds tag to tagField (use from "outside")
 	// update currentCompleteTags
 	[currentCompleteTagsInField addObjectsFromArray:tokens];
 	
+	NSLog(@"%@",currentCompleteTagsInField);
+	
 	// Update manage files flag if first tag was entered
 	[self updateManageFilesFlagOnTaggableObjects];
 	
