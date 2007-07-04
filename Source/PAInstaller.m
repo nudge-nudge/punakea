708de591051e62b44754bfe9bd35f93b5c1e345e
johannes <johannes@nudgenudge.eu>
2007-07-04 16:11:52 +0000

    spotlight importer is now located in correct directory (app bundle)

--- Source/PAInstaller.m
+++ Source/PAInstaller.m
@@ -18,54 +18,55 @@
 
 + (void)install
 {
-	[PAInstaller copyWeblocImporter];
+	//[PAInstaller copyWeblocImporter];
 }
 
-+ (void)copyWeblocImporter
-{
-	NSFileManager *fm = [NSFileManager defaultManager];
-	
-	// get path of webloc importer bundle
-	NSString *importerPath = [NSBundle pathForResource:@"WeblocImporter"
-												ofType:@"mdimporter"
-										   inDirectory:[[NSBundle mainBundle] builtInPlugInsPath]];
-	
-	NSString *targetPath = [@"~/Library/Spotlight" stringByExpandingTildeInPath];
-	BOOL isDirectory;
-	
-	// create dir if it does not exits
-	if (![fm fileExistsAtPath:targetPath isDirectory:&isDirectory])
-	{
-		[fm createDirectoryAtPath:targetPath attributes:nil];
-	}
-	else if (!isDirectory)
-	{
-		NSLog(@"critical error: Please make sure that '%@' is a directory",targetPath);
-		return;
-	}
-
-	// targetPath is now a directory
-	NSString *target = [targetPath stringByAppendingPathComponent:[importerPath lastPathComponent]];
-	
-	if ([fm fileExistsAtPath:target])
-	{
-		// check creation date
-		NSDictionary *targetAttribute = [fm fileAttributesAtPath:target traverseLink:NO];
-		NSDate *targetCreation = [targetAttribute objectForKey:NSFileCreationDate];
-		
-		NSDictionary *sourceAttribute = [fm fileAttributesAtPath:importerPath traverseLink:NO];
-		NSDate *sourceCreation = [sourceAttribute objectForKey:NSFileCreationDate];
-		
-		if ([targetCreation compare:sourceCreation] == NSOrderedAscending)
-		{
-			[fm removeFileAtPath:target handler:NULL];
-			[fm copyPath:importerPath toPath:target handler:NULL];	
-		}			
-	}
-	else
-	{
-		[fm copyPath:importerPath toPath:target handler:NULL];	
-	}
-}
+// not used at the moment
+//+ (void)copyWeblocImporter
+//{
+//	NSFileManager *fm = [NSFileManager defaultManager];
+//	
+//	// get path of webloc importer bundle
+//	NSString *importerPath = [NSBundle pathForResource:@"WeblocImporter"
+//												ofType:@"mdimporter"
+//										   inDirectory:[[NSBundle mainBundle] builtInPlugInsPath]];
+//	
+//	NSString *targetPath = [@"~/Library/Spotlight" stringByExpandingTildeInPath];
+//	BOOL isDirectory;
+//	
+//	// create dir if it does not exits
+//	if (![fm fileExistsAtPath:targetPath isDirectory:&isDirectory])
+//	{
+//		[fm createDirectoryAtPath:targetPath attributes:nil];
+//	}
+//	else if (!isDirectory)
+//	{
+//		NSLog(@"critical error: Please make sure that '%@' is a directory",targetPath);
+//		return;
+//	}
+//
+//	// targetPath is now a directory
+//	NSString *target = [targetPath stringByAppendingPathComponent:[importerPath lastPathComponent]];
+//	
+//	if ([fm fileExistsAtPath:target])
+//	{
+//		// check creation date
+//		NSDictionary *targetAttribute = [fm fileAttributesAtPath:target traverseLink:NO];
+//		NSDate *targetCreation = [targetAttribute objectForKey:NSFileCreationDate];
+//		
+//		NSDictionary *sourceAttribute = [fm fileAttributesAtPath:importerPath traverseLink:NO];
+//		NSDate *sourceCreation = [sourceAttribute objectForKey:NSFileCreationDate];
+//		
+//		if ([targetCreation compare:sourceCreation] == NSOrderedAscending)
+//		{
+//			[fm removeFileAtPath:target handler:NULL];
+//			[fm copyPath:importerPath toPath:target handler:NULL];	
+//		}			
+//	}
+//	else
+//	{
+//		[fm copyPath:importerPath toPath:target handler:NULL];	
+//	}
+//}
 
 @end
