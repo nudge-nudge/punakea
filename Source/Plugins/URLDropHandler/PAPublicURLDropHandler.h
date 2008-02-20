ef76fdfed955b81f73e4647b197c73cb532d4b59
johannes <johannes@nudgenudge.eu>
2008-02-20 13:26:12 +0000

    dropping firefox (3) bookmarks now working

---
+++ Source/Plugins/URLDropHandler/PAPublicURLDropHandler.h
@@ -0,0 +1,19 @@
+//
+//  PAPublicUrlDropHandler.h
+//  punakea
+//
+//  Created by Johannes Hoffart on 20.02.08.
+//  Copyright 2008 nudge:nudge. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "PADropHandler/PADropHandler.h"
+#import "PAURLDropDataHandler.h";
+
+
+
+@interface PAPublicURLDropHandler : PADropHandler {
+
+}
+
+@end
---
+++ Source/Plugins/URLDropHandler/PAPublicURLDropHandler.m
@@ -0,0 +1,53 @@
+//
+//  PAPublicUrlDropHandler.m
+//  punakea
+//
+//  Created by Johannes Hoffart on 20.02.08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "PAPublicURLDropHandler.h"
+
+
+@implementation PAPublicURLDropHandler
+
+- (id)init
+{
+	if (self = [super init])
+	{
+		pboardType = [@"public.url" retain];
+		dataHandler = [[PAURLDropDataHandler alloc] init];
+	}
+	return self;
+}
+
+- (BOOL)willHandleDrop:(NSPasteboard*)pasteboard
+{
+	NSString *url = [pasteboard stringForType:pboardType];
+	
+	if (url)
+		return YES;
+	else
+		return NO;
+}
+
+- (NSArray*)handleDrop:(NSPasteboard*)pasteboard
+{
+	NSString *url = [pasteboard stringForType:pboardType];
+	NSString *urlTitle = [pasteboard stringForType:@"public.url-name"];
+		
+	NSDictionary *urlDictionary = [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects:url,urlTitle,nil]
+																  forKeys:[NSArray arrayWithObjects:@"url",@"title",nil]];
+		
+	return [NSArray arrayWithObject:[dataHandler fileDropData:urlDictionary]];
+}
+
+- (NSDragOperation)performedDragOperation:(NSPasteboard*)pasteboard
+{
+	if ([self willHandleDrop:pasteboard])
+		return [dataHandler performedDragOperation];
+	else
+		return NSDragOperationNone;
+}
+
+@end
