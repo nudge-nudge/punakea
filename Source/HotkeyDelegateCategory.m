c61f82dfe57709e6bb86cd7338d099482e9ec47e
daniel <daniel@nudgenudge.eu>
2009-03-13 22:00:18 +0000

    Finished: Capturing of current selection now working for Finder, Safari, Firefox (buggy AS support), Opera

--- Source/HotkeyDelegateCategory.m
+++ Source/HotkeyDelegateCategory.m
@@ -33,23 +33,60 @@
 
 - (void)hitTaggerHotkey:(PTHotKey *)hotKey
 {
-	NSLog(@"Hit Me, Baby!");
-	
-	// Get selected items from Finder	
 	NSString *scriptPath = [[NSBundle mainBundle] pathForResource:@"CurrentSelection" ofType:@"scpt"];
 	NSURL *scriptURL = [NSURL fileURLWithPath:scriptPath];	
 	
 	NSAppleScript *script = [[[NSAppleScript alloc] initWithContentsOfURL:scriptURL error:nil] autorelease];
-	NSAppleEventDescriptor *descriptor = [script executeAndReturnError:nil];
 	
-	NSMutableArray *items = [NSMutableArray array];
+	NSMutableDictionary *err = [NSMutableDictionary dictionary];
+	NSAppleEventDescriptor *descriptor = [script executeAndReturnError:&err];
 	
-	// Note: AppleScript arrays are one-based!
-	for (int i = 1; i <= [descriptor numberOfItems]; i++)
+	[self showTagger:self];
+	TaggerController *tagger = [self taggerController];
+	
+	/* Note: AppleScript arrays are one-based! (OMFG)
+	   We're using the following structure of the return value:
+	   { 1: Files as array,
+	     2: One URL as array, first argument is location, second is title }
+	*/	
+		
+	// Handle files	
+	NSAppleEventDescriptor *filesDescriptor = [descriptor descriptorAtIndex:1];
+	
+	for (int i = 1; i <= [filesDescriptor numberOfItems]; i++)
 	{
-		NSLog(@"%@", [[descriptor descriptorAtIndex:i] stringValue]);	
-		[items addObject:[[descriptor descriptorAtIndex:i] stringValue]];
+		NSString *location = [[filesDescriptor descriptorAtIndex:i] stringValue];
+		
+		[tagger addTaggableObject:[NNFile fileWithPath:location]];
 	}
+	
+	// Handle URL
+	NSAppleEventDescriptor *urlDescriptor = [descriptor descriptorAtIndex:2];
+		
+	if ([urlDescriptor numberOfItems] == 2)
+	{
+		NSString *location = [[urlDescriptor descriptorAtIndex:1] stringValue];
+		NSString *title = [[urlDescriptor descriptorAtIndex:2] stringValue];
+		
+		// Check on a valid title - FF3 is buggy and returns only an empty string
+		if ([title isEqualToString:@""])
+			title = location;
+		
+		// Convert URL to webloc file		
+		NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
+		
+		[pasteboard declareTypes:[NSArray arrayWithObject:NSURLPboardType]
+						   owner:nil];			
+		
+		[pasteboard setString:location
+					  forType:@"public.url"];
+		[pasteboard setString:title
+					  forType:@"public.url-name"];
+
+		NSArray *files = [[PADropManager sharedInstance] handleDrop:pasteboard];
+		
+		[tagger addTaggableObjects:files];	
+	}		
 }
 
 - (PTHotKey *)taggerHotkey
