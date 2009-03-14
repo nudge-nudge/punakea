e8794fd53abd2bbdefc1e0ab14f6e13f274cbc92
daniel <daniel@nudgenudge.eu>
2009-03-14 14:03:53 +0000

    Punakea is now scriptable! Now allows for hotkey to trigger selection even from within Punakea itself.

---
+++ Source/AppleScriptDelegateCategory.h
@@ -0,0 +1,17 @@
+//
+//  AppleScriptDelegateCategory.h
+//  punakea
+//
+//  Created by Daniel on 14.03.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "Core.h"
+
+
+@interface Core (AppleScriptDelegateCategory)
+
+- (NSArray *)selection;
+
+@end
---
+++ Source/AppleScriptDelegateCategory.m
@@ -0,0 +1,52 @@
+//
+//  AppleScriptDelegateCategory.m
+//  punakea
+//
+//  Created by Daniel on 14.03.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppleScriptDelegateCategory.h"
+
+
+@implementation Core (AppleScriptDelegateCategory)
+
+- (BOOL)application:(NSApplication *)sender 
+ delegateHandlesKey:(NSString *)key
+{
+    if ([key isEqual:@"selection"]) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+/**
+ Get the currently selected files
+ @return List of file paths
+ */
+- (NSArray *)selection
+{
+	NSMutableArray *results = [NSMutableArray array];
+	
+	if ([self appHasBrowser]) 
+	{
+		PABrowserViewMainController *mainController = [[browserController browserViewController] mainController];
+		
+		if ([mainController isKindOfClass:[PAResultsViewController class]])
+		{
+			PAResultsOutlineView *ov = [(PAResultsViewController*)mainController outlineView];
+			
+			for (int i = 0; i < [[ov selectedItems] count]; i++)
+			{
+				NNFile *file = [[ov selectedItems] objectAtIndex:i];
+				
+				[results addObject:[file path]];
+			}
+		}
+	}
+	
+	return results;
+}
+
+@end
